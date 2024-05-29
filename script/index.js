ce80312cf8b50387dbbda0ce7d2d3bec9864dd4b
unknown <shahinafridi012@gmail.com>
2024-05-29 18:11:29 +0600

    70% done surely

---
+++ script/index.js
@@ -0,0 +1,104 @@
+// the hide The element of home functional work here
+function BookingForm(){
+    const hideTheElement = document.getElementById('destination');
+    hideTheElement.classList.add('hidden');
+    const hideTheSecond = document.getElementById('header-container');
+    hideTheSecond.classList.add('hidden');
+    const Ph_parAbiYan = document.getElementById('ph-parabiyan')
+    Ph_parAbiYan.classList.add('hidden')
+}
+function colorButton(){
+    const ColorTheButton = document.getElementById('button')
+    ColorTheButton.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+
+    
+
+}
+function colorButton_1(){
+    const ColorTheButton_1 = document.getElementById('button-1')
+    ColorTheButton_1.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+    
+}
+function colorButton_2(){
+    const ColorTheButton_2 = document.getElementById('button_2')
+    ColorTheButton_2.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+}
+function colorButton_3(){
+    const ColorTheButton_3 = document.getElementById('button_3')
+    ColorTheButton_3.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+    
+}
+function colorButton_4(){
+    const ColorTheButton_4 = document.getElementById('button_4')
+    ColorTheButton_4.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+}
+function colorButton_5(){
+    const ColorTheButton_5 = document.getElementById('button_5')
+    ColorTheButton_5.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+}
+function colorButton_6(){
+    const ColorTheButton_6 = document.getElementById('button_6')
+    ColorTheButton_6.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+}
+function colorButton_7(){
+    const ColorTheButton_7 = document.getElementById('button_7')
+    ColorTheButton_7.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+}
+function colorButton_8(){
+    const ColorTheButton_8 = document.getElementById('button_8')
+    ColorTheButton_8.classList.add('bg-emerald-400');
+    const currentScoreElement = document.getElementById('total-price');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    const newScore = currentScore + 550;
+    currentScoreElement.innerText = newScore;
+}
